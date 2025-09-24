31e313b07ae425a68987d78c41801ac028ccc827
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-09-24 15:22:02 -0600

    Creación de proyecto + primer script
    Son miles de archivos debido a Unity

---
+++ MAS simulador Fisica2/Assets/Scripts/src_MASimulador.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class src_MASimulador : MonoBehaviour
+{
+      //variables principales (públicas y privadas)
+    public float amplitud = 2f;
+    public float masa = 1f;
+    public float k = 10f;
+    public float fifi = 0f;
+    private float omega;
+    private float tiempo;
+    
+    void Start()
+    {
+        omega = Mathf.Sqrt(k/masa); //fórmula frecuencia angular con Mathf
+        tiempo = 0; //inicializar el tiempo en 0s
+    }
+
+    
+    void Update()
+    {
+        tiempo += Time.deltaTime; //tiempo transcurrido
+
+        float x = amplitud * Mathf.Cos(omega * tiempo + fifi); //fórmula de posición
+        transform.position = new Vector3(x, transform.position.y, 0); //actualizar pos del obj
+    }
+}
