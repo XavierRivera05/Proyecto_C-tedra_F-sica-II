8e5a81aa9c042cc13997dd498d129c730187a808
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-09-26 08:47:35 -0600

    Resorte visual con LineRenderer
    Me costó XD

--- MAS simulador Fisica2/Assets/Scripts/Visual/src_visualresorte.cs
+++ MAS simulador Fisica2/Assets/Scripts/Visual/src_visualresorte.cs
@@ -7,7 +7,7 @@ public class src_visualresorte : MonoBehaviour
     //variables de tipo transform para colocar otros objetos como valor
     public Transform puntofijo;
     public Transform masita;
-    public Transform resortevisual;
+    public LineRenderer resortevisual;
 
     void Start()
     {
@@ -17,15 +17,24 @@ public class src_visualresorte : MonoBehaviour
     
     void Update()
     {
-        //posicionar el resorte visual en el centro del obj_masa y punto fijo
-        resortevisual.position = (masita.position + puntofijo.position) / 2;
+        int curva = 20; //número de curvas del resorte 
+        resortevisual.positionCount = curva;
 
-        //usar un Vector para ajustar el sprite(imagen)
-        Vector3 direction = masita.position - puntofijo.position;
-        resortevisual.up = direction.normalized;
+        //configuración del Vector 3 (posicion visual)
+        Vector3 start = puntofijo.position;
+        Vector3 end = masita.position;
+        Vector3 direction = (end - start).normalized;
+        float longitud = Vector3.Distance(start,end);
 
-        //Escalar el sprite para que se estire
-        float distancia = direction.magnitude;
-        resortevisual.localScale = new Vector3(1, distancia, 1);
+        for (int i = 0; i < curva; i++)
+        {
+            float tito = (float)i / (curva - 1);
+            Vector3 pos = Vector3.Lerp(start,end, tito);
+
+            //crear una "ondulación" con la función seno para simular un resorte chino
+            pos += Vector3.Cross(direction, Vector3.forward) * Mathf.Sin(tito * Mathf.PI * 20) * 0.1f; //el pi sirve para los ciclos, y el 0.1f es amplitud
+
+            resortevisual.SetPosition(i, pos);
+        }
     }
 }
